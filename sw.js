f31b6de75e799699a76e0311dc1f367302c7c525
gaboruiz84 <abner.ruiz.martinez@clases.edu.sv>
2026-03-07 13:28:11 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cuotas-ceca-v9';
+const CACHE_NAME = 'cuotas-ceca-v10';
 
 // Archivos estáticos que queremos guardar en el teléfono para que cargue rápido (y funcione offline la interfaz)
 const urlsToCache = [
